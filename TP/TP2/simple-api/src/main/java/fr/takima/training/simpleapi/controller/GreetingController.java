e08c2821531de7b6eb95dbb254d62105b9968a46
Dorian Bouquin <dorian.bouquin@epfedu.fr>
2025-05-22 12:04:58 +0200

    Again

--- TP/TP2/simple-api/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
+++ TP/TP2/simple-api/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
@@ -16,7 +16,6 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
     }
 
-    @GetMapping("/deploy")
     public String deploy() {
         return "Deployment Successful";
     }
